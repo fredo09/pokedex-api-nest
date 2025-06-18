17bfdabda240253e5f84296d0e509c3288c7999c
fredo09 <fred_15@outlook.com>
2025-06-17 23:14:31 -0600

    Configurar el prefijo global para las rutas de la API a 'api/v2'

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //! Configurar el prefijo global para las rutas de la API ejemplo: /api/v2
+  app.setGlobalPrefix('api/v2');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
