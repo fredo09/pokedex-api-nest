5c3da9ccc37c85e21b61f527e1eb49eba133e811
fredo09 <fred_15@outlook.com>
2025-06-18 00:15:40 -0600

    Agregar esquema de Mongoose para la entidad Pokémon y configurar el módulo correspondiente

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,16 @@
-export class Pokemon {}
+import { Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+ //! decorador para definir un esquema de Mongoose
+@Schema()
+export class Pokemon extends Document {
+	 //! decorador para definir una propiedad del esquema
+	@Prop({ unique: true, index: true })
+	name: string;
+
+	@Prop({ unique: true, index: true })
+	pokemon_number: number;
+}
+
+ //! crea un esquema de Mongoose a partir de la clase Pokemon
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([ //* Importa el esquema de Mongoose para la entidad Pokemon
+      { name: Pokemon.name, schema: PokemonSchema }
+    ])
+  ]
 })
 export class PokemonModule {}
