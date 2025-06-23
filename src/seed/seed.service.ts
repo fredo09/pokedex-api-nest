24f4717be648ead5a4593f0d0b767d2239513480
fredo09 <fred_15@outlook.com>
2025-06-22 23:35:36 -0600

    Actualizar método executeSeed en SeedService para limitar la cantidad de Pokémon a 1 y registrar el ID de cada Pokémon en la consola

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,12 +4,19 @@ import { PokeAPIResponseI } from './interface';
 
 @Injectable()
 export class SeedService {
-
   //! Forma provisional para realizar peticiones HTTP usando axios
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeAPIResponseI>('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const { data } = await this.axios.get<PokeAPIResponseI>('https://pokeapi.co/api/v2/pokemon?limit=1');
+
+    data?.results.forEach(({name, url}) => {
+      const pokemonId: number = +url.split('/').slice(-2, -1)[0];
+      console.log("🚀 ~ SeedService ~ data?.results.forEach ~ pokemonId:", {
+        name,
+        numero_pokemon: pokemonId,
+      })
+    })
 
     return data?.results;
   }
