67cad2120ffd12bee4f0cf82c843063fb425a8f6
fredo09 <fred_15@outlook.com>
2025-06-23 00:54:15 -0600

    Actualizar método executeSeed en SeedService para eliminar Pokémon existentes y realizar inserciones masivas en la base de datos "Forma de hacerlo mas eficiente"

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -17,16 +17,42 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeAPIResponseI>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    //! Constante para insertar los pokemons en la base de datos forma uno
+    // const arrayPromiseInsertPokemon: Promise<any>[] = [];
+
+    const pokemonsToinsert: { name: string; pokemon_number: number }[] = [];
+
+    //* Tendremos que eliminar los pokemons existentes en la base de datos
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeAPIResponseI>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     data?.results.forEach( async({name, url}) => {
       const pokemonId: number = +url.split('/').slice(-2, -1)[0];
 
-      const result = await this.pokemonModel.create({
-        name: name.toLowerCase(),
-        pokemon_number: pokemonId,
-      });
+      //! Forma alternativa de realizar la insercion de los pokemons en la base de datos
+      // const result = await this.pokemonModel.create({
+      //   name: name.toLowerCase(),
+      //   pokemon_number: pokemonId,
+      // });
+
+      //* Forma uno de hacer la insercion de los pokemons en la base de datos
+      // arrayPromiseInsertPokemon.push(
+      //   this.pokemonModel.create({
+      // name: name.toLowerCase(),
+      // pokemon_number: pokemonId,
+      //   })
+      // );
+
+      //! Forma dos de hacer la insercion de los pokemons en la base de datos
+      pokemonsToinsert.push({ name: name.toLowerCase(), pokemon_number: pokemonId })
     });
+
+    //! forma de hacer una insercion masiva de los pokemons
+    // await Promise.all(arrayPromiseInsertPokemon);
+
+    //! Forma dos de hacer una insercion masiva de los pokemons "Recomendado"
+    await this.pokemonModel.insertMany(pokemonsToinsert);
 
     // * Insertamos los pokemons en la base de datos */
     // await this.pokemonService.create(dataTransformed);
