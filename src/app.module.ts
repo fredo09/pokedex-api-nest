c08b3d816ea90e313defc08b8fa9685cbfa4f45f
fredo09 <fred_15@outlook.com>
2025-06-29 23:17:34 -0600

    Agregar soporte para variables de entorno y configurar el módulo de configuración en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -13,11 +14,19 @@ import { SeedModule } from './seed/seed.module';
       rootPath: join(__dirname,'..', 'public')
     }),
 
+    //! Confirgurar dependencia "config" PARA PODER LEER VARIABLES DE ENTORNO
+    //! https://docs.nestjs.com/techniques/configuration
+    ConfigModule.forRoot(),
+
     //! Importar el modulo de mongoose
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-poke-api'),
+    MongooseModule.forRoot(process.env.MONGODB_URL || ''),
     PokemonModule,
     CommonModule,
     SeedModule
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log("🚀 ~ contructor:", process.env);
+  }
+}
